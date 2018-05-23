c3b1ae3d23159342ddae70455ae43d9c2226b6aa
Shaham Hai <shahamh@gmail.com>
2018-05-23 16:16:10 +0300

    created animalDAO

---
+++ src/dao/AnimalDAO.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+class AnimalDAO {
+    constructor(){
+        this.animalUrl = 'http://159.89.100.98/api/animals';
+    }
+    createAnimal(animal){
+        return axios.put(this.animalUrl, animal);
+    }
+    getAllAnimals(){
+        return axios.get(this.animalUrl);
+    }
+    updateAnimal(animal){
+        return axios.post(this.animalUrl, animal);
+    }
+    deleteAnimalById(id){
+        return axios.delete(this.animalUrl + '/' + id);
+    }
+}
